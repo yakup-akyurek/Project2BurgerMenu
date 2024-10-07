9e27e09bac495e0cf99d6baccd968ceac7bb222d
yakup akyürek <akyurek3466@icloud.com>
2024-10-07 18:59:03 +0300

    Add project files.

---
+++ Project2BurgerMenu/Context/BurgerMenuContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using Project2BurgerMenu.Entities;
+
+namespace Project2BurgerMenu.Context
+{
+    public class BurgerMenuContext:DbContext
+
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<DealOfTheDay> DealOfTheDays { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+
+    }
+}
