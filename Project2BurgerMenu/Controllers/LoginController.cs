caf51dbcc4d025e37e52a5c6f09471ad5d697355
yakup akyürek <akyurek3466@icloud.com>
2024-10-17 00:25:46 +0300

    Step 2

---
+++ Project2BurgerMenu/Controllers/LoginController.cs
@@ -0,0 +1,39 @@
+﻿using Project2BurgerMenu.Context;
+using Project2BurgerMenu.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace Project2BurgerMenu.Controllers
+{
+    //admin girişi
+    public class LoginController : Controller
+    {
+        BurgerMenuContext context = new BurgerMenuContext();        
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Admin admin)
+        {
+            var values=context.Admins.FirstOrDefault(x=>x.Username==admin.Username && x.Password==admin.Password);
+            if (values != null)
+            {
+                FormsAuthentication.SetAuthCookie(values.Username,false);
+                Session["x"]=values.Username.ToString();
+                return RedirectToAction("ProdcutList","Product","Admin");
+
+            }
+            else 
+            {
+                return View();
+            }
+
+        }
+    }
+}
